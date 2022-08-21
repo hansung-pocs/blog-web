66671c38e90f5beeeb85d321b59bb56a80d3408b
Junghayeon <jennyjhy@naver.com>
2022-08-21 16:08:03 +0900

    메인페이지 카테고리별 목록 변경

--- js/app.js
+++ js/app.js
@@ -41,9 +41,43 @@ function moveLoginPage(){
 
 }
 
+let url;
+
+//홈페이지에 인기글 최근글 3개 불러오기
+const best_card = document.querySelector(".best-card");
+url = "http://34.64.161.55:8001/posts?id=best&offset=3&pageNum=1";
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        best_card.innerHTML = "";
+        if (data.data === null) {
+            best_card.innerHTML = "<tr><td>0</td><td>글을 작성하세요.</td><td></td></tr>";
+        } else {
+            let cnt=0;
+            for (let i = 0; i < data.data.posts.length; i++) {
+                if(cnt>2)
+                    break;
+                if(1){
+                    best_card.innerHTML +=
+                        `
+                    <div class="col-xl">
+                        <div class="card">
+                            <div class="card-header">${data.data.posts[i].title}</div>
+                            <div class="card-body">${data.data.posts[i].writerName} - ${data.data.posts[i].createdAt}</div>
+                           <div class="card-footer">${data.data.posts[i].content}</div>
+                        </div>
+                    </div>
+                     `;
+                    cnt++;
+                }
+            }
+        }
+    })
+
 //홈페이지에 공지사항 최근글 3개 불러오기
 const notice_card = document.querySelector(".notice-card");
-const url = "http://34.64.161.55:8001/posts?id=notice&offset=3&pageNum=1";
+url = "http://34.64.161.55:8001/posts?id=notice&offset=3&pageNum=1";
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -73,3 +107,99 @@ fetch(url)
         }
     })
 
+    
+//홈페이지에 스터디 최근글 3개 불러오기
+const study_card = document.querySelector(".study-card");
+url = "http://34.64.161.55:8001/posts?id=study&offset=3&pageNum=1";
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        study_card.innerHTML = "";
+        if (data.data === null) {
+            study_card.innerHTML = "<tr><td>0</td><td>글을 작성하세요.</td><td></td></tr>";
+        } else {
+            let cnt=0;
+            for (let i = 0; i < data.data.posts.length; i++) {
+                if(cnt>2)
+                    break;
+                if(data.data.posts[i].category==="study"){
+                    study_card.innerHTML +=
+                        `
+                    <div class="col-xl">
+                        <div class="card">
+                            <div class="card-header">${data.data.posts[i].title}</div>
+                            <div class="card-body">${data.data.posts[i].writerName} - ${data.data.posts[i].createdAt}</div>
+                           <div class="card-footer">${data.data.posts[i].content}</div>
+                        </div>
+                    </div>
+                     `;
+                    cnt++;
+                }
+            }
+        }
+    })
+       
+//홈페이지에 추억 최근글 3개 불러오기
+const memory_card = document.querySelector(".memory-card");
+url = "http://34.64.161.55:8001/posts?id=memory&offset=3&pageNum=1";
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        memory_card.innerHTML = "";
+        if (data.data === null) {
+            memory_card.innerHTML = "<tr><td>0</td><td>글을 작성하세요.</td><td></td></tr>";
+        } else {
+            let cnt=0;
+            for (let i = 0; i < data.data.posts.length; i++) {
+                if(cnt>2)
+                    break;
+                if(data.data.posts[i].category==="memory"){
+                    memory_card.innerHTML +=
+                        `
+                    <div class="col-xl">
+                        <div class="card">
+                            <div class="card-header">${data.data.posts[i].title}</div>
+                            <div class="card-body">${data.data.posts[i].writerName} - ${data.data.posts[i].createdAt}</div>
+                           <div class="card-footer">${data.data.posts[i].content}</div>
+                        </div>
+                    </div>
+                     `;
+                    cnt++;
+                }
+            }
+        }
+    })
+       
+//홈페이지에 노하우 최근글 3개 불러오기
+const knowhow_card = document.querySelector(".knowhow-card");
+url = "http://34.64.161.55:8001/posts?id=knowhow&offset=3&pageNum=1";
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        knowhow_card.innerHTML = "";
+        if (data.data === null) {
+            knowhow_card.innerHTML = "<tr><td>0</td><td>글을 작성하세요.</td><td></td></tr>";
+        } else {
+            let cnt=0;
+            for (let i = 0; i < data.data.posts.length; i++) {
+                if(cnt>2)
+                    break;
+                if(data.data.posts[i].category==="knowhow"){
+                    knowhow_card.innerHTML +=
+                        `
+                    <div class="col-xl">
+                        <div class="card">
+                            <div class="card-header">${data.data.posts[i].title}</div>
+                            <div class="card-body">${data.data.posts[i].writerName} - ${data.data.posts[i].createdAt}</div>
+                           <div class="card-footer">${data.data.posts[i].content}</div>
+                        </div>
+                    </div>
+                     `;
+                    cnt++;
+                }
+            }
+        }
+    })
