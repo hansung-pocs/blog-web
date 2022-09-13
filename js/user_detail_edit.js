17414ac1bf5620503817a825233597b19ef56b05
DaeHyun Kwon <104804antonio@gmail.com>
2022-09-13 18:39:43 +0900

    프로필 이미지 기능 개발중

--- js/user_detail_edit.js
+++ js/user_detail_edit.js
@@ -19,6 +19,34 @@ const generation = document.querySelector("#user_detail_edit_generation");
 const company = document.querySelector("#user_detail_edit_company");
 const github = document.querySelector("#user_detail_edit_github");
 const editForm = document.querySelector("#editForm");
+const chooseFile = document.getElementById("chooseFile");
+
+function loadFile(input) {
+  var file = input.files[0]; //선택된 파일 가져오기
+  console.log(file);
+
+  //미리 만들어 놓은 div에 text(파일 이름) 추가
+  var newImage = document.getElementById("profileImg");
+  newImage.setAttribute("class", "img");
+
+  //이미지 source 가져오기
+  newImage.src = URL.createObjectURL(file);
+  console.log(newImage.src);
+  newImage.style.width = "200px";
+  newImage.style.height = "200px";
+
+  var cancelImage = document.getElementById("cancelImage");
+  cancelImage.classList.remove("hidden");
+}
+
+const cancelFile = () => {
+  var newImage = document.getElementById("profileImg");
+  newImage.src = "../img/logo.png";
+  chooseFile.value = null;
+  console.log(chooseFile);
+  var cancelImage = document.getElementById("cancelImage");
+  cancelImage.classList.add("hidden");
+};
 
 fetch(url, { headers: header })
   .then((response) => response.json())
@@ -81,6 +109,27 @@ editForm.addEventListener("submit", async function userEdit(event) {
   const result = await response.json();
   console.log(result);
 
+  //profile 업로드
+  let formData = new FormData();
+  formData.append("image", chooseFile.files[0]);
+
+  console.log(chooseFile.files[0]);
+
+  const profileOptions = {
+    method: "PATCH",
+    header: {
+      "x-pocs-session-token": sessionToken,
+    },
+    body: formData,
+  };
+
+  const imageResponse = await fetch(
+    `http://34.64.161.55:80/api/users/${id}/profile`,
+    profileOptions
+  );
+  const result2 = await imageResponse.json();
+  console.log(result2);
+
   if (result.status !== 302) {
     //에러 발생시
     window.location.href = "../html/user_detail.html?userId=" + id;
@@ -90,6 +139,48 @@ editForm.addEventListener("submit", async function userEdit(event) {
     window.location.href = "../html/user_detail.html?userId=" + id; ////편집후 바로 이전화면으로
   }
 });
+
+//저장버튼-업데이트
+// editForm.addEventListener("submit",async function userEdit(event){
+//     event.preventDefault();
+//     console.log('edit');
+
+//     const body=JSON.stringify({
+//         password: password.value,
+//         name:userName.value,
+//         email:email.value,
+//         github:github.value,
+//         company:company.value,
+//     });
+
+//     let formData = new FormData();
+//     formData.append('body', body);
+//     formData.append('files', chooseFile.files[0]);
+
+//     console.log(formData);
+
+//     const options = {
+//         method : 'PATCH',
+//         headers : {
+//             'x-pocs-session-token' : sessionToken
+//         },
+//         body : formData
+//     };
+
+//     const response = await fetch(`http://34.64.161.55:8001/users/${id}`, options);
+//     const result = await response.json();
+//     console.log(result);
+
+//     if(result.status !==302){ //에러 발생시
+//         window.location.href = '../html/user_detail.html?userId='+id;
+
+//     }
+//     else{ //잘 되었다면
+//         console.log(result.message);
+//         window.location.href = '../html/user_detail.html?userId='+id;////편집후 바로 이전화면으로
+//     }
+// });
+
 //유저 정보 수정을 취소하는 버튼 이벤트
 cancelBtn.addEventListener("click", function (event) {
   event.preventDefault();
